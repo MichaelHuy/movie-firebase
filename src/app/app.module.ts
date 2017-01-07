8a9ee48ef4d03086bd98c42db57073fc4b1d7d55
HuyLe <lehuydit@gmail.com>
2017-01-07 10:33:29 +0700

    First app with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,29 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// <script src="https://www.gstatic.com/firebasejs/3.6.4/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyAgIxkRI8pg0CTHQPmNBbEOCDF9OQjWRFw",
+//     authDomain: "kidmovies-734f4.firebaseapp.com",
+//     databaseURL: "https://kidmovies-734f4.firebaseio.com",
+//     storageBucket: "kidmovies-734f4.appspot.com",
+//     messagingSenderId: "760238858260"
+//   };
+//   firebase.initializeApp(config);
+// </script>
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyAgIxkRI8pg0CTHQPmNBbEOCDF9OQjWRFw",
+  authDomain: "kidmovies-734f4.firebaseapp.com",
+  databaseURL: "https://kidmovies-734f4.firebaseio.com",
+  storageBucket: "kidmovies-734f4.appspot.com",
+  messagingSenderId: "760238858260"
+};
 @NgModule({
   declarations: [
     MyApp,
@@ -15,7 +38,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
